025a42b8fe77943b6df1e2d854cd725d8b7d9921
Michael Gerdes <mgerdes11@gmail.com>
2015-09-10 21:13:01 -0400

    The 'hard' part of the kata

--- adder_new_lang/michaelgerdes/adder-test.rkt
+++ adder_new_lang/michaelgerdes/adder-test.rkt
@@ -1,14 +1,45 @@
 #lang racket/base
 
-(require rackunit
-         "adder.rkt")
+(require rackunit "adder.rkt")
 
-(check-equal? (adder "") 0 "It works with the empty string")
-(check-equal? (adder "1") 1 "It works with a single number")
-(check-equal? (adder "1,2") 3 "It works with two numbers")
-(check-equal? (adder "1,2,3") 6 "It works with three numbers")
-(check-equal? (adder "1,2,3,4,5,6,7,8,9,10") (+ 1 2 3 4 5 6 7 8 9 10) "It works with lots of numbers")
-(check-equal? (adder "2,5,9") 16 "It still works when not adding consecutive numbers.")
-(check-equal? (adder "2\n5,9") 16 "It don't care if a newline is a delimiter.")
-(check-equal? (adder "//:2\n5,9:8") 24 "It don't care if you define your own delimiter.")
-(check-equal? (adder "//252525") 15 "Even a number can be a delimiter.")
+(check-equal? (adder "") 0 
+              "It works with the empty string")
+
+(check-equal? (adder "1") 1 
+              "It works with a single number")
+
+(check-equal? (adder "1,2") 3 
+              "It works with two numbers")
+
+(check-equal? (adder "1,2,3") 6 
+              "It works with three numbers")
+
+(check-equal? (adder "1,2,3,4,5,6,7,8,9,10") (+ 1 2 3 4 5 6 7 8 9 10) 
+              "It works with lots of numbers")
+
+(check-equal? (adder "2,5,9") 16 
+              "It still works when not adding consecutive numbers.")
+
+(check-equal? (adder "2\n5,9") 16 
+              "It don't care if a newline is a delimiter.")
+
+(check-equal? (adder "//:\n2\n5,9:8") 24 
+              "It don't care if you define your own delimiter.")
+
+(check-equal? (adder "//2\n52525") 15 
+              "Even a number can be a delimiter.")
+
+(check-equal? (adder "10,20,1001") 30 
+              "Numbers larger than 1000 are ignored.")
+
+(check-equal? (adder "10,20,1000") 1030 
+              "The number 1000 is not ignored though.")
+
+(check-equal? (adder "//[***]\n1***2***3") 6
+              "It works for custom delimiters of any length.")
+
+(check-equal? (adder "//[***][&&&]\n1***2***3&&&4") 10
+              "It works for multiple custom delimiters of any length.")
+
+(check-equal? (adder "//[111][222][333]\n4111422243334") 16
+              "Lol this is weird.")
--- adder_new_lang/michaelgerdes/adder.rkt
+++ adder_new_lang/michaelgerdes/adder.rkt
@@ -4,37 +4,89 @@
 (define (adder numbers)
   (define delimiters (list "," "\n"))
 
-  (define (index-of-first-delimiter numbers)
-    (let ([index-of-delimiters (filter (lambda (index) 
-                                         (not (equal? #f index)))  
-                                       (map (lambda (delimiter) 
-                                              (string-contains numbers delimiter)) 
-                                            delimiters))])
-      (if (zero? (length index-of-delimiters))
-        #f
-        (apply min index-of-delimiters))))
+  (define (delimiter-index delimiter)
+    (car delimiter))
 
-  (define (custom-delimiter? numbers)
-    (equal? #\/ (string-ref numbers 0)))
+  (define (delimiter-length delimiter)
+    (cadr delimiter))
 
-  (define (add-to-delimiters delimiter)
-    (set! delimiters (cons delimiter delimiters)))
+  (define (custom-delimiter?) 
+    (equal? (string-ref numbers 0) #\/))
 
-  (define (adder-iter accumulation numbers)
-    (let ([index-of-first-delimiter (index-of-first-delimiter numbers)])
-      (if index-of-first-delimiter
-        (let ([first-number (string->number 
-                              (substring numbers 0 index-of-first-delimiter))]
-              [rest-of-numbers (substring numbers (+ 1 index-of-first-delimiter))])
-          (adder-iter (+ accumulation first-number) rest-of-numbers))
-        (+ accumulation (string->number numbers)))))
+  (define (first-custom-delimiter custom-delimiters)
+    (substring custom-delimiters 0 (string-contains custom-delimiters "]")))
+
+  (define (rest-custom-delimiters custom-delimiters)
+    (let ([potential-delimiter (substring custom-delimiters (string-contains custom-delimiters "]"))])
+      (if (equal? (string-ref potential-delimiter 1) #\newline)
+        null 
+        (substring potential-delimiter 2))))
+
+  (define (add-multi-length-custom-delimiters custom-delimiters)
+    (if (null? custom-delimiters)
+      null
+      (begin
+        (set! delimiters (cons (first-custom-delimiter custom-delimiters) 
+                               delimiters))
+        (add-multi-length-custom-delimiters (rest-custom-delimiters custom-delimiters)))))
+
+  (define (add-custom-delimiters)
+    (if (equal? (string-ref numbers 2) #\[)
+      (add-multi-length-custom-delimiters (substring numbers 3))
+      (set! delimiters (cons (string (string-ref numbers 2)) delimiters))))
+
+  (define (numbers-without-custom-delimiters)
+    (substring numbers (+ (string-contains numbers "\n") 1)))
+
+  (define (min-indexed-delimiter delimiters)
+    (if (null? delimiters)
+      #f
+      (foldl (lambda (smallest-delimiter delimiter)
+               (if (< (delimiter-index delimiter) (delimiter-index smallest-delimiter))
+                 delimiter
+                 smallest-delimiter))
+             (car delimiters)
+             (cdr delimiters))))
+
+  (define (first-delimiter numbers)
+    (min-indexed-delimiter 
+      (filter (lambda (delimiter)
+                (not (equal? #f delimiter)))  
+              (map (lambda (delimiter)
+                     (let ([index (string-contains numbers delimiter)])
+                       (if index 
+                         (list index (string-length delimiter)) 
+                         #f))) 
+                   delimiters))))
+
+  (define (first-number numbers)
+    (let ([first-delimiter (first-delimiter numbers)])
+      (let ([first-number (if first-delimiter
+                            (string->number (substring numbers 0 (delimiter-index first-delimiter)))
+                            (string->number numbers))])
+        (if (> first-number 1000)
+          0
+          first-number))))
+
+  (define (rest-numbers numbers)
+    (let ([first-delimiter (first-delimiter numbers)])
+      (if first-delimiter
+        (substring numbers (+ (delimiter-index first-delimiter) 
+                              (delimiter-length first-delimiter)))
+        null)))
+
+  (define (adder-iter acc numbers)
+    (if (null? numbers)
+      acc
+      (adder-iter (+ acc (first-number numbers))
+                  (rest-numbers numbers))))
 
   (if (zero? (string-length numbers))
-    0
-    (if (custom-delimiter? numbers)
+    0 
+    (if (custom-delimiter?)
       (begin
-        (add-to-delimiters (string (string-ref numbers 2)))
-        (adder-iter 0 (substring numbers 3)))
-      (adder-iter 0 numbers)))) 
+        (add-custom-delimiters) 
+        (adder-iter 0 (numbers-without-custom-delimiters)))
+      (adder-iter 0 numbers))))
 
 (provide adder)
